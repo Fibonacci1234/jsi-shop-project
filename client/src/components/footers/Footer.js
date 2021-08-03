67f0a3836ec7ab0c45d8d1cb2b86aef9e7d9c58d
Dương <70809746+Duong-pog@users.noreply.github.com>
2021-08-03 22:11:17 +0700

    bootstrap footer

--- client/src/components/footers/Footer.js
+++ client/src/components/footers/Footer.js
@@ -2,9 +2,19 @@ import React from 'react'
 
 function Footer(){
     return(
-        <div>
-            Footer
-        </div>
+        <div class="footer-basic">
+        <footer>
+            <div class="social"><a href="#"><i class="icon ion-social-instagram"></i></a><a href="#"><i class="icon ion-social-snapchat"></i></a><a href="#"><i class="icon ion-social-twitter"></i></a><a href="#"><i class="icon ion-social-facebook"></i></a></div>
+            <ul class="list-inline">
+                <li class="list-inline-item"><a href="#">Home</a></li>
+                <li class="list-inline-item"><a href="/cart">Cart</a></li>
+                <li class="list-inline-item"><a href="/history">History</a></li>
+                <li class="list-inline-item"><a href="#">Terms</a></li>
+                <li class="list-inline-item"><a href="#">Privacy Policy</a></li>
+            </ul>
+            <p class="copyright">TĐ STORE © 2021</p>
+        </footer>
+    </div>
     )
 }
 
